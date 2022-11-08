4684154b8b80bc385d751081aa32e56b2c97a58a
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-08 16:51:37 -0500

    v0.13.1 Se integra secciones key y cer en CSD

--- controllers/controlador_fc_csd.php
+++ controllers/controlador_fc_csd.php
@@ -27,8 +27,23 @@ class controlador_fc_csd extends \gamboamartin\facturacion\controllers\controlad
         $this->sidebar['modifica']['titulo'] = "Modifica CSD";
         $this->sidebar['modifica']['stepper_active'] = true;
         $this->sidebar['modifica']['menu'] = array(
-            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_alta,menu_lateral_active: true),
-            $this->menu_item(menu_item_titulo: "Nueva Seccion", link: $this->link_alta,menu_seccion_active: true));
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_modifica,menu_lateral_active: true),
+            $this->menu_item(menu_item_titulo: "Subir Key", link: $this->link_fc_key_csd,menu_seccion_active: true),
+            $this->menu_item(menu_item_titulo: "Subir Cer", link: $this->link_fc_cer_csd,menu_seccion_active: true));
+
+        $this->sidebar['subir_key']['titulo'] = "Modifica CSD";
+        $this->sidebar['subir_key']['stepper_active'] = true;
+        $this->sidebar['subir_key']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_modifica,menu_seccion_active: true),
+            $this->menu_item(menu_item_titulo: "Subir Key", link: $this->link_fc_key_csd,menu_lateral_active: true),
+            $this->menu_item(menu_item_titulo: "Subir Cer", link: $this->link_fc_cer_csd,menu_seccion_active: true));
+
+        $this->sidebar['subir_cer']['titulo'] = "Modifica CSD";
+        $this->sidebar['subir_cer']['stepper_active'] = true;
+        $this->sidebar['subir_cer']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_modifica,menu_seccion_active: true),
+            $this->menu_item(menu_item_titulo: "Subir Key", link: $this->link_fc_key_csd,menu_seccion_active: true),
+            $this->menu_item(menu_item_titulo: "Subir Cer", link: $this->link_fc_cer_csd,menu_lateral_active: true));
 
     }
 
---
+++ views/fc_csd/subir_key.php
@@ -0,0 +1,40 @@
+<?php /** @var \tglobally\tg_facturacion\controllers\controlador_fc_csd $controlador */ ?>
+
+<?php (new \tglobally\template_tg\template())->sidebar($controlador); ?>
+
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+
+        <div class="  form-main" id="form">
+            <form method="post" action="<?php echo $controlador->link_fc_key_csd_alta_bd;?>" class="form-additional" enctype="multipart/form-data">
+                <?php echo $controlador->inputs->fc_csd_id; ?>
+                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->codigo_bis; ?>
+                <?php echo $controlador->inputs->documento; ?>
+                <div class="buttons col-md-12">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="subir_key">Guarda</button>
+                    </div>
+                    <div class="col-md-6 ">
+                        <a href="<?php echo $controlador->link_lista; ?>"  class="btn btn-info btn-guarda col-md-12 ">Lista</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="lista">
+            <div class="card">
+                <div class="card-header">
+                    <span class="text-header">Registro de Keys</span>
+                </div>
+                <div class="card-body">
+                    <div class="cont_tabla_sucursal  col-md-12">
+                        <table id="fc_key_csd" class="table table-striped" ></table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
