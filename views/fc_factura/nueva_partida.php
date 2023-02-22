b2d97001b1067fc7c728206caae4b86cb8e708bd
Kevin <kevin.acuna@ivitec.com.mx>
2023-02-22 17:43:22 -0500

    v0.21.2 Se integra alta partida en nueva seccion de de factura

--- views/fc_factura/nueva_partida.php
+++ views/fc_factura/nueva_partida.php
@@ -1,6 +1,6 @@
 <?php /** @var \tglobally\tg_facturacion\controllers\controlador_fc_factura $controlador */ ?>
 
-<?php (new \tglobally\template_tg\template())->sidebar($controlador); ?>
+<?php (new \tglobally\template_tg\template())->sidebar(controlador:$controlador, seccion_step: 2); ?>
 
 <div class="col-md-9 formulario">
     <div class="col-lg-12">
@@ -9,6 +9,17 @@
 
         <div class="  form-main" id="form">
             <form method="post" action="<?php echo $controlador->link_modifica_bd;?>" class="form-additional">
+
+                <?php echo $controlador->inputs->fc_factura_id; ?>
+                <?php echo $controlador->inputs->com_producto_id; ?>
+                <?php echo $controlador->inputs->unidad; ?>
+                <?php echo $controlador->inputs->impuesto; ?>
+                <?php echo $controlador->inputs->descripcion; ?>
+                <?php echo $controlador->inputs->cantidad; ?>
+                <?php echo $controlador->inputs->valor_unitario; ?>
+                <?php echo $controlador->inputs->subtotal; ?>
+                <?php echo $controlador->inputs->descuento; ?>
+                <?php echo $controlador->inputs->total; ?>
 
 
                 <div class="buttons col-md-12">
